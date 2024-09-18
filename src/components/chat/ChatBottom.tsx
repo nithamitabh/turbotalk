598f693669a6ee03944c206cd47904955c90aca1
nithamitabh <21dec004@nith.ac.in>
2024-09-19 01:01:01 +0530

    feat: Add Cloudinary integration and message skeleton component

--- src/components/chat/ChatBottom.tsx
+++ src/components/chat/ChatBottom.tsx
@@ -2,17 +2,25 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Image as ImageIcon, Loader, SendHorizontal, ThumbsUp } from "lucide-react";
 import Image from "next/image";
 import { Textarea } from "../ui/textarea";
-import { useEffect, useRef, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import EmojiPicker from "./EmojiPicker";
 import { Button } from "../ui/button";
 import useSound from "use-sound";
 import { usePreferences } from "@/store/usePrefrence";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CldUploadWidget, CloudinaryUploadWidgetInfo } from 'next-cloudinary';
+import { sendMessageAction } from "@/actions/message.action";
+import { useSelectedUser } from "@/store/useSelectedUser";
+import { pusherClient } from "@/lib/pusher";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { Message } from "@/db/dummy";
 const ChatBottom = () => {
-  const isPending = false;
+  const [ImgUrl,setImgUrl] = useState("");
   const [message, setMessage] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const { selectedUser } = useSelectedUser();
+  const {user:currentUser} = useKindeBrowserClient();
   const [playSound1] = useSound("/sounds/keystroke1.mp3");
   const [playSound2] = useSound("/sounds/keystroke2.mp3");
   const [playSound3] = useSound("/sounds/keystroke3.mp3");
@@ -21,18 +29,97 @@ const ChatBottom = () => {
   const [playNotificationSound] = useSound("/sounds/notification.mp3");
   const playSoundFunctions = [playSound1, playSound2, playSound3, playSound4];
 
+  const queryClient = useQueryClient();
+  const { mutate: sendMessage, isPending } = useMutation({
+    mutationFn: sendMessageAction,
+  });
+
+  const handleSendMessage = () => {
+    if (!message.trim()) return;
+    sendMessage({ content: message, messageType: "text", receiverId: selectedUser?.id! });
+    setMessage("");
+    textAreaRef.current?.focus();
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+			handleSendMessage();
+		}
+
+		if (e.key === "Enter" && e.shiftKey) {
+			e.preventDefault();
+			setMessage(message + "\n");
+		}
+	};
   const playRandomKeyStrokeSound = () => {
     const randomIndex = Math.floor(Math.random() * playSoundFunctions.length);
     soundEnabled && playSoundFunctions[randomIndex]();
   };
+
+  useEffect(() => {
+		const channelName = `${currentUser?.id}__${selectedUser?.id}`.split("__").sort().join("__");
+		const channel = pusherClient?.subscribe(channelName);
+
+		const handleNewMessage = (data: { message: Message }) => {
+			queryClient.setQueryData(["messages", selectedUser?.id], (oldMessages: Message[]) => {
+				return [...oldMessages, data.message];
+			});
+
+			if (soundEnabled && data.message.senderId !== currentUser?.id) {
+				playNotificationSound();
+			}
+		};
+
+		channel.bind("newMessage", handleNewMessage);
+
+		// ! Absolutely important, otherwise the event listener will be added multiple times which means you'll see the incoming new message multiple times
+		return () => {
+			channel.unbind("newMessage", handleNewMessage);
+			pusherClient.unsubscribe(channelName);
+		};
+	}, [currentUser?.id, selectedUser?.id, queryClient, playNotificationSound, soundEnabled]);
+
   return (
     <div className='p-2 flex justify-between w-full items-center gap-2'>
-      {!message.trim() && <ImageIcon
-        size={20}
-        onClick={() => open()}
-        className='cursor-pointer text-muted-foreground'
-      />}
+      {!message.trim() && (
+        <CldUploadWidget signatureEndpoint={"/api/sign-cloudinary-params"} onSuccess={(result,{widget}) => {
+          setImgUrl((result.info as CloudinaryUploadWidgetInfo).secure_url);
+          widget.close();
+        }}>
+          {({ open }) => {
+            return (
+              <ImageIcon
+              size={20}
+              onClick={() => open()}
+              className='cursor-pointer text-muted-foreground'
+            />
+            )
+          }}
+        </CldUploadWidget>
+      )}
       <AnimatePresence>
+      <Dialog open={!!ImgUrl}>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Image Preview</DialogTitle>
+					</DialogHeader>
+					<div className='flex justify-center items-center relative h-96 w-full mx-auto'>
+						<Image src={ImgUrl} alt='Image Preview' fill className='object-contain' />
+					</div>
+
+					<DialogFooter>
+						<Button
+							type='submit'
+							onClick={() => {
+								sendMessage({ content: ImgUrl, messageType: "image", receiverId: selectedUser?.id!});
+								setImgUrl("");
+							}}
+						>
+							Send
+						</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
         <motion.div
           layout
           initial={{ opacity: 0, scale: 1 }}
@@ -54,7 +141,7 @@ const ChatBottom = () => {
             className='w-full border rounded-full flex items-center h-9 resize-none overflow-hidden
 						bg-background min-h-0'
             value={message}
-            // onKeyDown={handleKeyDown}
+            onKeyDown={handleKeyDown}
             onChange={(e) => {
               setMessage(e.target.value);
               playRandomKeyStrokeSound();
@@ -73,32 +160,32 @@ const ChatBottom = () => {
           </div>
         </motion.div>
         {message.trim() ? (
-					<Button
-						className='h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0'
-						variant={"ghost"}
-						size={"icon"}
-						// onClick={handleSendMessage}
-					>
-						<SendHorizontal size={20} className='text-muted-foreground' />
-					</Button>
-				) : (
-					<Button
-						className='h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0'
-						variant={"ghost"}
-						size={"icon"}
-					>
-						{!isPending && (
-							<ThumbsUp
-								size={20}
-								className='text-muted-foreground'
-								// onClick={() => {
-								// 	sendMessage({ content: "👍", messageType: "text", receiverId: selectedUser?.id! });
-								// }}
-							/>
-						)}
-						{isPending && <Loader size={20} className='animate-spin' />}
-					</Button>
-				)}
+          <Button
+            className='h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0'
+            variant={"ghost"}
+            size={"icon"}
+          onClick={handleSendMessage}
+          >
+            <SendHorizontal size={20} className='text-muted-foreground' />
+          </Button>
+        ) : (
+          <Button
+            className='h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0'
+            variant={"ghost"}
+            size={"icon"}
+          >
+            {!isPending && (
+              <ThumbsUp
+                size={20}
+                className='text-muted-foreground'
+              onClick={() => {
+              	sendMessage({ content: "👍", messageType: "text", receiverId: selectedUser?.id! });
+              }}
+              />
+            )}
+            {isPending && <Loader size={20} className='animate-spin' />}
+          </Button>
+        )}
 
       </AnimatePresence>
     </div>
